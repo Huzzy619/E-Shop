18c1122d8e696c644ed52a7ec69f71c40dba0211
Huzzy619 <blazingkrane@gmail.com>
2023-04-04 09:40:34 +0100

    resolved price- used ORM to get Sum

--- shop/models.py
+++ shop/models.py
@@ -4,16 +4,16 @@ from uuid import uuid4
 
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericRelation
-from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.db.models import Q, Sum
 
 from likes.models import Like
 from shop.validators import validate_file_size
 
 
 class Color(models.Model):
-    name = models.CharField(max_length=200, null=True, blank= True )
+    name = models.CharField(max_length=200, null=True, blank=True)
     hex_code = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self) -> str:
@@ -32,7 +32,7 @@ class Size(models.Model):
 class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey(
-            "Product", on_delete=models.SET_NULL, null=True, related_name="+", blank=True
+        "Product", on_delete=models.SET_NULL, null=True, related_name="+", blank=True
     )
 
     def __str__(self) -> str:
@@ -45,14 +45,12 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
-    unit_price = models.DecimalField(
-            max_digits=6, decimal_places=2
-    )
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField(validators=[MinValueValidator(0)])
     date_posted = models.DateTimeField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(
-            Collection, on_delete=models.PROTECT, related_name="products"
+        Collection, on_delete=models.PROTECT, related_name="products"
     )
     likes = GenericRelation(Like)
     # colors = models.ManyToManyField(Color, blank=True)
@@ -88,7 +86,7 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey(
-            Product, on_delete=models.CASCADE, related_name="images"
+        Product, on_delete=models.CASCADE, related_name="images"
     )
     image = models.ImageField(upload_to="store/images", validators=[validate_file_size])
     # colors = models.ManyToManyField(Color, blank=True)
@@ -96,11 +94,15 @@ class ProductImage(models.Model):
 
 
 class SizeInventory(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="size_inventory")
-    size = models.ForeignKey(Size, on_delete=models.CASCADE, related_name="product_size")
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="size_inventory"
+    )
+    size = models.ForeignKey(
+        Size, on_delete=models.CASCADE, related_name="product_size"
+    )
     quantity = models.IntegerField(default=0, blank=True)
     extra_price = models.DecimalField(
-            max_digits=6, decimal_places=2, blank=True, null=True
+        max_digits=6, decimal_places=2, blank=True, null=True
     )
 
     class Meta:
@@ -121,28 +123,32 @@ class SizeInventory(models.Model):
 
 
 class ColorInventory(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="color_inventory")
-    color = models.ForeignKey(Color, on_delete=models.CASCADE, related_name="product_color")
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="color_inventory"
+    )
+    color = models.ForeignKey(
+        Color, on_delete=models.CASCADE, related_name="product_color"
+    )
     quantity = models.IntegerField(default=0, blank=True)
     extra_price = models.DecimalField(
-            max_digits=6, decimal_places=2, blank=True, null=True
+        max_digits=6, decimal_places=2, blank=True, null=True
     )
 
     class Meta:
-        verbose_name_plural = "Product Color & Inventories" 
+        verbose_name_plural = "Product Color & Inventories"
 
     def __str__(self):
         return self.product.title
 
     # def clean(self):
-        # total_quantity = self.product.color_inventory.aggregate(
-        #         total_quantity=models.Sum('quantity'))['total_quantity'] or 0
-        # if total_quantity + self.quantity > self.product.inventory:
-        #     raise ValidationError(
-        #             "Total quantity of this product size inventory exceeds the amount in stock.")
-        # if self.quantity == 0:
-        #     raise ValidationError(
-        #             "This product size is no more in stock.")
+    # total_quantity = self.product.color_inventory.aggregate(
+    #         total_quantity=models.Sum('quantity'))['total_quantity'] or 0
+    # if total_quantity + self.quantity > self.product.inventory:
+    #     raise ValidationError(
+    #             "Total quantity of this product size inventory exceeds the amount in stock.")
+    # if self.quantity == 0:
+    #     raise ValidationError(
+    #             "This product size is no more in stock.")
 
 
 class Order(models.Model):
@@ -157,7 +163,7 @@ class Order(models.Model):
     id = models.CharField(primary_key=True, max_length=10)
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(
-            max_length=10, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING
+        max_length=10, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING
     )
     customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
     shipping_address = models.CharField(blank=True, null=True, max_length=1000)
@@ -177,21 +183,21 @@ class Order(models.Model):
         return "#" + value
 
     def get_total_price(self):
-        return sum(
-                [item.quantity * item.unit_price for item in self.items.all()]
-        )
+        return sum([item.quantity * item.unit_price for item in self.items.all()])
 
     class Meta:
         permissions = [("cancel_order", "Can cancel order")]
+
+
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name="items")
     product = models.ForeignKey(
-            Product, on_delete=models.PROTECT, related_name="orderitems"
+        Product, on_delete=models.PROTECT, related_name="orderitems"
     )
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     size = models.CharField(max_length=100, null=True, blank=True)
-    color = models.CharField( max_length=100, null=True, blank=True)
+    color = models.CharField(max_length=100, null=True, blank=True)
 
 
 class TrackOrder(models.Model):
@@ -202,14 +208,12 @@ class TrackOrder(models.Model):
     ]
 
     order = models.OneToOneField(
-            Order, on_delete=models.CASCADE, related_name="tracking"
+        Order, on_delete=models.CASCADE, related_name="tracking"
     )
     status = models.CharField(choices=ORDER_STATUS, default="checking", max_length=200)
     checking_date = models.DateTimeField(auto_now_add=True)
     in_transit_date = models.DateTimeField(null=True, blank=True)
     date_delivered = models.DateTimeField(null=True, blank=True)
-
-
 
 
 class BillingAddress(models.Model):
@@ -228,42 +232,33 @@ class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField(validators=[MinValueValidator(1)])
     size = models.CharField(max_length=100, null=True, blank=True)
-    color = models.CharField( max_length=100, null=True, blank=True)
+    color = models.CharField(max_length=100, null=True, blank=True)
 
     # class Meta:
     #     unique_together = [["cart", "product"]]
     @property
     def resolved_price(self):
         main_price = self.product.unit_price
-        colors_price =  []
-        sizes_price = []
 
-        colors = self.product.color_inventory.filter(color__name = self.color)
-        if colors:
-            for item in colors:
-                if item.extra_price:
-                    colors_price.append(item.extra_price)
-        
-        sizes = self.product.size_inventory.filter(size__size = self.size)
-        if sizes:
-            for item in sizes:
-                if item.extra_price:
-                    sizes_price.append(item.extra_price)
-        
-        total = main_price + sum(sizes_price) + sum(colors_price)
+        colors_price = self.product.color_inventory.filter(
+            ~Q(extra_price=None), color__name=self.color
+        ).aggregate(c_total=Sum("extra_price"))["c_total"]
 
-        return total
+        sizes_price = self.product.size_inventory.filter(
+            ~Q(extra_price=None), size__size=self.size
+        ).aggregate(s_total=Sum("extra_price"))["s_total"]
 
+        prices = [main_price, sizes_price, colors_price]
 
-
+        return sum([price for price in prices if price is not None])
 
 
 class Review(models.Model):
     product = models.ForeignKey(
-            Product, on_delete=models.CASCADE, related_name="reviews"
+        Product, on_delete=models.CASCADE, related_name="reviews"
     )
     rating = models.IntegerField(
-            validators=[MaxValueValidator(5), MinValueValidator(1)]
+        validators=[MaxValueValidator(5), MinValueValidator(1)]
     )
     description = models.TextField()
     date = models.DateField(auto_now_add=True)
@@ -271,10 +266,10 @@ class Review(models.Model):
 
 class Notification(models.Model):
     NOTIFICATION_TYPE_CHOICES = (
-    ("OFFER", "OFFER"),
-    ("FEED", "FEED"),
-    ("ACTIVITY", "ACTIVITY"),
-)
+        ("OFFER", "OFFER"),
+        ("FEED", "FEED"),
+        ("ACTIVITY", "ACTIVITY"),
+    )
     id = models.UUIDField(default=uuid4, unique=True, editable=False, primary_key=True)
     type = models.CharField(max_length=200, choices=NOTIFICATION_TYPE_CHOICES)
     title = models.CharField(max_length=200, null=True)
