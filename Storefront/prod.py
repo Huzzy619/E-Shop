2cd7fa4b8579740eda2fbab1242cb1d94f6f2859
Huzzy619 <blazingkrane@gmail.com>
2023-04-07 12:19:27 +0100

    Changes

--- Storefront/prod.py
+++ Storefront/prod.py
@@ -22,7 +22,16 @@ DATABASES = {
 INSTALLED_APPS.remove("debug_toolbar")
 MIDDLEWARE.remove("debug_toolbar.middleware.DebugToolbarMiddleware")
 
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
+STORAGES = {
+    # ...
+    "default":{
+        "BACKEND":"cloudinary_storage.storage.MediaCloudinaryStorage"
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 CLOUDINARY_STORAGE = {
     "CLOUD_NAME": config("CLOUD_NAME", ""),
