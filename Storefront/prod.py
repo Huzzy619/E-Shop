ef490628c2b4896e60336176d691241cb2afabfb
s13g <ayflix0@gmail.com>
2023-04-08 16:11:25 +0100

    linode

--- Storefront/prod.py
+++ Storefront/prod.py
@@ -6,7 +6,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = config("DEBUG", False, cast=bool)
 
-ALLOWED_HOSTS = ["139.162.145.4", "e-commerce.cleverapps.io", "eshop.cleverapps.io"]
+ALLOWED_HOSTS = ["172.104.133.142", "e-commerce.cleverapps.io", "eshop.cleverapps.io"]
 
 CSRF_TRUSTED_ORIGINS = ["https://" + host for host in ALLOWED_HOSTS]
 
